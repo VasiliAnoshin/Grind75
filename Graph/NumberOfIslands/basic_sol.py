8533c8f58540167375b8e685a2c59b5dd9f6db92
BenjaminFranklin <anoshin45@gmail.com>
2023-11-29 17:10:22 +0200

    add Number of Islands

---
+++ Graph/NumberOfIslands/basic_sol.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid) -> int:
+        visited, num_islands = set(), 0
+        rows = len(grid)
+        cols = len(grid[0])
+        def bfs(x, y, grid):
+            q = deque()
+            q.append((x,y))
+            visited.add((x, y))
+            
+            while q:
+                x, y = q.popleft()
+                directions = [(0,1), (1,0), (-1,0), (0,-1)]
+                for r,c in directions:
+                    if (r+x in range(rows) and 
+                        c+y in range(cols) and 
+                        grid[r+x][c+y] == '1' and 
+                        (r+x, c+y) not in visited):
+                            q.append((r+x,c+y))
+                            visited.add((r+x,c+y))
+            
+        
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == '1' and (x,y) not in visited:
+                    bfs(x,y,grid)
+                    num_islands += 1
+        return num_islands
